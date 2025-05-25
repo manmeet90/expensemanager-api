5aaaf4f3c1d6d7fc18037e06746335aca9099045
Manmeet Gupta <manmeet.g7@gmail.com>
2025-05-25 14:44:25 +0530

    Update loginController.js

--- server/controller/loginController.js
+++ server/controller/loginController.js
@@ -3,9 +3,9 @@ const {User} = require('../models');
 class LoginController {
 
     login(username, password) {
-        let passwordbtoa = Buffer.from(password).toString("base64");
+        //let passwordbtoa = Buffer.from(password).toString("base64");
         return new Promise((resolve, reject) => { 
-            User.findOne({username:username, password:passwordbtoa})
+            User.findOne({username:username.toLowerCase(), password:password})
             .exec((err, doc) => {
                 if(err) {
                     reject(err);
